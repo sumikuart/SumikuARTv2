7089c70a8af9d12b5cea666a50f5adf045b63ab1
sumikuart <smikku@hotmail.com>
2019-03-02 13:56:17 +0100

    forside opsætning færdig, men mangler adgang til API 02-03-2019

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// var ServicesURL = "api/art.jason";
+
+
+// callAPI()
+
+// function callAPI() {
+
+//     fetch(ServicesURL).then(function (respons) {
+
+//         return respons.json();
+
+//     }).then(function (jsonRespons) {
+
+//         //Se det fulde API du har hentet, og hvilke dataer du kan hente. 
+//         console.log(jsonRespons);
+
+//         //dette er hvad der skal ske med dataen vi for. 
+//         //useAPIdata(jsonRespons);
+
+
+//     }).catch(function (error) {
+//         //hvis der sker fejl udføre den denne kode.
+//         alert(error);
+//     });
+
+// }
